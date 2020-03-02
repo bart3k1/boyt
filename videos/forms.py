f13b2ef3f63add2c13b09c29c3777c6cc58dca22
bart3k1 <bart3k1@gmail.com>
2020-03-02 20:14:11 +0100

    dashboard and add movie added + ajax search with youtube api

--- videos/forms.py
+++ videos/forms.py
@@ -5,9 +5,10 @@ from django import forms
 class MoviesForm(forms.ModelForm):
     class Meta:
         model = Movies
-        fields = ["title", "url", "youtube_id"]
-        labels = {"youtube_id": "YouTube Id"}
+        # fields = ["title", "url", "youtube_id"]
+        fields = ["url"]
+        labels = {"url": "YouTube Url:"}
 
 
 class SearchForm(forms.Form):
-    search_term = forms.CharField(max_length=255, label="Search for Videos")
+    search_term = forms.CharField(max_length=255, label="Search for Videos:")
--- videos/views.py
+++ videos/views.py
@@ -1,11 +1,18 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.http import Http404, JsonResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from videos.models import Row, Movies
 from .forms import MoviesForm, SearchForm
 from django.forms import formset_factory
+import urllib
+import requests
+from django.forms.utils import ErrorList
+
+YOUTUBE_API_KEY = "AIzaSyBeUCedZda4PQu1CyG-8oJTRNZa_5kRc50"
 
 
 def home(request):
@@ -13,7 +20,24 @@ def home(request):
 
 
 def dashboard(request):
-    return render(request, "videos/dashboard.html")
+    rows = Row.objects.filter(user=request.user)
+    return render(request, "videos/dashboard.html", {"rows": rows})
+
+
+class DeleteMovie(generic.DeleteView):
+    model = Movies
+    template_name = "videos/delete_movie.html"
+    success_url = reverse_lazy("dashboard")
+
+
+def movie_search(request):
+    search_form = SearchForm(request.GET)
+    if search_form.is_valid():
+        encoded_search_term = urllib.parse.quote(search_form.cleaned_data["search_term"])
+        response = requests.get(
+            f"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=6&q={encoded_search_term}&key={YOUTUBE_API_KEY}")
+        return JsonResponse(response.json())
+    return JsonResponse({"error": "Form not validated"})
 
 
 def add_movie(request, pk):
@@ -22,52 +46,53 @@ def add_movie(request, pk):
     # form = video_form_set()
 
     search_form = SearchForm()
+    row = Row.objects.get(pk=pk)
+    if not row.user == request.user:
+        raise Http404
 
     if request.method == "POST":
-        filled_form = MoviesForm(request.POST)
+        form = MoviesForm(request.POST)
         # filled_form = video_form_set(request.POST)
-        if filled_form.is_valid():
-            #loop for formset and + changes in html
+        if form.is_valid():
+            # loop for formset and + changes in html
             # for form in filled_form:
             movie = Movies()
-            movie.url = filled_form.cleaned_data["url"]
-            movie.title = filled_form.cleaned_data["title"]
-            movie.youtube_id = filled_form.cleaned_data["youtube_id"]
-            movie.row = Row.objects.get(pk=pk)
-            movie.save()
-            return redirect("home")
-
+            movie.row = row
+            movie.url = form.cleaned_data["url"]
+            # movie.title = filled_form.cleaned_data["title"]
+            # movie.youtube_id = filled_form.cleaned_data["youtube_id"]
+            parsed_url = urllib.parse.urlparse(movie.url)
+            movie_id = urllib.parse.parse_qs(parsed_url.query).get("v")
+            if movie_id:
+                movie.youtube_id = movie_id[0]
+                response = requests.get(
+                    f"https://www.googleapis.com/youtube/v3/videos?part=snippet&id={movie_id[0]}&key={YOUTUBE_API_KEY}")
+                json = response.json()
+                title = json["items"][0]["snippet"]["title"]
+                movie.title = title
+                movie.save()
+                return redirect("detail_row", pk)
+            else:
+                errors = form._errors.setdefault("url", ErrorList())
+                errors.append("Needs to be youtube url")
     else:
         pass
 
-    return render(request, "videos/add_movie.html", {"form": form, "search_form": search_form})
+    return render(request, "videos/add_movie.html", {"form": form, "search_form": search_form, "row": row})
 
 
-class SignUp(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("home")
-    template_name = "registration/signup.html"
-
-    # add automatic login after signup
-    def form_valid(self, form):
-        view = super(SignUp, self).form_valid(form)
-        username, password = form.cleaned_data.get("username"), form.cleaned_data.get("password1"),
-        user = authenticate(username=username, password=password)
-        login(self.request, user)
-        return view
-
-
+# @login_required(login_url="/signup")
 class CreateRow(generic.CreateView):
     model = Row
     fields = ["title"]
     template_name = "videos/create_row.html"
-    success_url = reverse_lazy("home")
+    success_url = reverse_lazy("dashboard")
 
     # adding user data to the form
     def form_valid(self, form):
         form.instance.user = self.request.user
         super(CreateRow, self).form_valid(form)
-        return redirect("home")
+        return redirect("dashboard")
 
 
 class DetailRow(generic.DetailView):
@@ -86,3 +111,19 @@ class DeleteRow(generic.DeleteView):
     model = Row
     template_name = "videos/delete_row.html"
     success_url = reverse_lazy("dashboard")
+
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("home")
+    template_name = "registration/signup.html"
+
+    # add automatic login after signup
+    def form_valid(self, form):
+        view = super(SignUp, self).form_valid(form)
+        username, password = form.cleaned_data.get("username"), form.cleaned_data.get("password1"),
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return view
+
+
